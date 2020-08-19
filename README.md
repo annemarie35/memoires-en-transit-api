06e5b4764bacc0548665a7c526f8f6ce5737f635
Anne-Marie <amesteves@octo.com>
2020-08-19 16:26:21 +0200

    j'ai ajouté un premier modèle de données avec alembic

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 Installer les dépendances : `pip3 install -r requirements.txt`
 Lancer le serveur : `python app.py`
+
+
+`export FLASK_APP=app.py` permet de lancer l'app avec la commande `flask run`
+
+# Mettre à jour le modèle de donnée
+
+Ajouter la class model voulue dans app.py
+
+puis  `python manage.py db migrate` génère la migrations/version
+`python manage.py db upgrade` ou downgrade
--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask, request
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+from settings import load_environment_variables
+from sqlalchemy import Column, Integer, String
+
+
 app = Flask(__name__)
 
+load_environment_variables()
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class LieuModel(db.Model):
+    __tablename__ = 'lieu'
+
+    id = Column(Integer, primary_key=True)
+    nom = Column(String(140), nullable=False)
+    latitude = Column(db.Numeric(8, 5), nullable=True)
+    longitude = Column(db.Numeric(8, 5), nullable=True)
+
+    def __init__(self, nom, latitude, longitude):
+        self.nom = nom
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"<Lieu {self.nom}>"
 
 @app.route("/")
 def hello():
